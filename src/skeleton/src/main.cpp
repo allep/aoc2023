22a20336b5f35f87eed07184bd16789b94e17a5d
Alessandro Paganelli <alessandro.paganelli@gmail.com>
2023-12-02 16:43:19 +0100

    WIP: skeleton added (still to test)

---
+++ src/skeleton/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "main_include.h"
+
+int main() {
+    std::cout << "Hello " << ProgramName << "!" << std::endl;
+    return 0;
+}
---
+++ src/skeleton/src/main_include.h
@@ -0,0 +1,1 @@
+constexpr char ProgramName[] = "Day";
